f4ea4db678a72da7eeb6e7d672d407168723e279
LUPUS001 <antoniodaw23@gmail.com>
2023-12-19 10:09:24 +0100

    Actualización

--- src/PaisCapital.java
+++ src/PaisCapital.java
@@ -1,25 +1,34 @@
 import java.util.*;
 public class PaisCapital {
     public static void main(String[] args) {
-        Map<String, String> paisesMap = new HashMap<String, String>();
-        paisesMap.put("España", "Madrid");
-        paisesMap.put("Alemania", "Berlin");
-        paisesMap.put("Italia", "Roma");
-        paisesMap.put("Noruega", "Oslo");
-        paisesMap.put("Francia", "Paris");
+        Map<String, String> paises = new HashMap<String, String>();
+        paises.put("Canada", "Ottawa");
+        paises.put("España", "Madrid");
+        paises.put("Italia", "Roma");
+        paises.put("Argentina", "Buenos Aires");
+        paises.put("Francia", "Paris");
+        paises.put("Ucrania", "Kiev");
+        paises.put("Rusia", "Moscú");
+        paises.put("México", "Ciudad de México");
+        paises.put("EEUU", "Washington D. C");
+        paises.put("Perú", "Lima");
+        paises.put("Venezuela", "Caracas");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Dime un pais Europeo");
-        String capitalesMap = sc.nextLine();
-        if (paisesMap.containsKey(capitalesMap)){
-            System.out.println("La capital de " + capitalesMap + " es " + paisesMap.get(capitalesMap));
+        System.out.println("Países");
+        System.out.print("-");
+        for (String capitales: paises.keySet()) {
+            System.out.print(capitales + "-");
         }
-        else {
-            System.out.println("No tenemos la capital de ese país en el código");
-        }
+        System.out.println();
 
-    }
-    public static String capital (Map<String, String> paisesMap, String capitalesMap){
-        return paisesMap.get(capitalesMap);
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Dime un pais y te diré su capital");
+        String capitalesMap = sc.next();
+
+        if(paises.containsKey(capitalesMap)){
+            System.out.println("La capital de " + capitalesMap + " es " + paises.get(capitalesMap));
+        } else {
+            System.out.println("Ese pais o no existe o no lo tenemos en nuestra base de datos");
+        }
     }
 }
